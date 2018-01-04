ea22adcb477319cd85046411718801461d3ba0ea
Gianmarco Callegher <854612@stud.unive.it>
2018-01-04 18:27:27 +0100

    Created DBHelper

---
+++ HospitalWaste/template/app/src/main/java/it/unive/dais/cevid/datadroid/template/DatabaseUtils/DBHelper.java
@@ -0,0 +1,108 @@
+package it.unive.dais.cevid.datadroid.template.DatabaseUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by gianmarcocallegher on 04/01/18.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "HospitalWaste.db";
+
+    private HashMap hp;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(
+                "CREATE TABLE Appalti (" +
+                        "cig TEXT NOT NULL," +
+                        "oggetto TEXT," +
+                        "aggiudicatario TEXT," +
+                        "[c.f.] TEXT NOT NULL," +
+                        "ragione_sociale TEXT," +
+                        "importo REAL NOT NULL," +
+                        "codice_ente TEXT REFERENCES ULSS (codice_ente) ON DELETE CASCADE ON UPDATE CASCADE MATCH SIMPLE NOT NULL," +
+                        "PRIMARY KEY ( cig ASC, codice_ente ASC )" +
+                ");"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
+
+    public boolean insertContact (String name, String phone, String email, String street,String place) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        contentValues.put("street", street);
+        contentValues.put("place", place);
+        db.insert("contacts", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from contacts where id="+id+"", null );
+        return res;
+    }
+
+    public int numberOfRows(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, CONTACTS_TABLE_NAME);
+        return numRows;
+    }
+
+    public boolean updateContact (Integer id, String name, String phone, String email, String street,String place) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        contentValues.put("street", street);
+        contentValues.put("place", place);
+        db.update("contacts", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        return true;
+    }
+
+    public Integer deleteContact (Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("contacts",
+                "id = ? ",
+                new String[] { Integer.toString(id) });
+    }
+
+    public ArrayList<String> getAllCotacts() {
+        ArrayList<String> array_list = new ArrayList<String>();
+
+        //hp = new HashMap();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from contacts", null );
+        res.moveToFirst();
+
+        while(res.isAfterLast() == false){
+            array_list.add(res.getString(res.getColumnIndex(CONTACTS_COLUMN_NAME)));
+            res.moveToNext();
+        }
+        return array_list;
+    }
+}
