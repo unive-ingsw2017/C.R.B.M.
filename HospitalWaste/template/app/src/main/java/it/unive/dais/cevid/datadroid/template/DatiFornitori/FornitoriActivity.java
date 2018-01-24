57da72184719afe72eba5a1a64136200301fe77c
Gianmarco Callegher <854612@stud.unive.it>
2018-01-24 18:48:49 +0100

    Fornitori Search Functionality
    Fornitori Search Functionality. Fixed Text Submnit method DatiBilancioActivity

--- HospitalWaste/template/app/src/main/java/it/unive/dais/cevid/datadroid/template/DatiFornitori/FornitoriActivity.java
+++ HospitalWaste/template/app/src/main/java/it/unive/dais/cevid/datadroid/template/DatiFornitori/FornitoriActivity.java
@@ -1,14 +1,23 @@
 package it.unive.dais.cevid.datadroid.template.DatiFornitori;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatCallback;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import it.unive.dais.cevid.datadroid.template.DatabaseUtils.FornitoriHelper;
 import it.unive.dais.cevid.datadroid.template.R;
@@ -17,8 +26,9 @@ import it.unive.dais.cevid.datadroid.template.R;
  * Created by francescobenvenuto on 18/01/2018.
  */
 
-public class FornitoriActivity extends Activity implements AppCompatCallback {
+public class FornitoriActivity extends AppCompatActivity {
     private RecyclerViewAdapter adapter;
+    private List<String> fornitori;
     /**
      * Questo metodo viene chiamato quando questa activity parte.
      * l'intent deve essere inviato con anche l'oggetto ULSS in modo da sapere cosa
@@ -34,15 +44,14 @@ public class FornitoriActivity extends Activity implements AppCompatCallback {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         FornitoriHelper helper = new FornitoriHelper();
+        fornitori = helper.getFornitori();
         adapter = new RecyclerViewAdapter(
                 this.getApplicationContext(),
-                helper.getFornitori()
+                fornitori
         );
         recyclerView.setAdapter(adapter);
 
-        AppCompatDelegate delegate = AppCompatDelegate.create(this, this);
-        delegate.onCreate(savedInstanceState);
-        delegate.getSupportActionBar().setTitle(getString(R.string.fornitori_hardcoded));
+        getSupportActionBar().setTitle(getString(R.string.fornitori_hardcoded));
     }
 
     @Override
@@ -63,7 +72,7 @@ public class FornitoriActivity extends Activity implements AppCompatCallback {
      */
     @Override
     public void onSupportActionModeStarted(ActionMode mode) {
-
+        super.onSupportActionModeStarted(mode);
     }
 
     /**
@@ -73,7 +82,7 @@ public class FornitoriActivity extends Activity implements AppCompatCallback {
      */
     @Override
     public void onSupportActionModeFinished(ActionMode mode) {
-
+        super.onSupportActionModeFinished(mode);
     }
 
     /**
@@ -85,5 +94,41 @@ public class FornitoriActivity extends Activity implements AppCompatCallback {
     @Override
     public ActionMode onWindowStartingSupportActionMode(ActionMode.Callback callback) {
         return null;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuItem myMenuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) myMenuItem.getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                myMenuItem.collapseActionView();
+                filter(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void filter(String newText) {
+        List<String> filteredFornitori = new ArrayList<>();
+        for (String fornitore : fornitori) {
+            if (fornitore.toLowerCase().contains(newText.toLowerCase()))
+                filteredFornitori.add(fornitore);
+        }
+        adapter.filter(filteredFornitori);
     }
 }
