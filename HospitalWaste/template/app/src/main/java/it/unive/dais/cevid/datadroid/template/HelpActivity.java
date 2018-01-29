804cbc3946aac9d16a5a5e56ae4c234dac6b6e64
Gianmarco Callegher <854612@stud.unive.it>
2018-01-29 18:26:06 +0100

    Fixed some things on Search Area

--- HospitalWaste/template/app/src/main/java/it/unive/dais/cevid/datadroid/template/HelpActivity.java
+++ HospitalWaste/template/app/src/main/java/it/unive/dais/cevid/datadroid/template/HelpActivity.java
@@ -8,10 +8,18 @@ import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ViewSwitcher;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 public class HelpActivity extends AppCompatActivity {
     private ImageSwitcher imageSwitcher;
     private Button next;
     private Button previous;
+    private static final int MAX_IMGS = 10;
+    private List<Integer> images = Collections.EMPTY_LIST;
+    private int current_position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +29,11 @@ public class HelpActivity extends AppCompatActivity {
         next = (Button) findViewById(R.id.next);
         previous = (Button) findViewById(R.id.prev);
 
+        images = new ArrayList<>();
+        current_position = 0;
+
         imageSwitcher = (ImageSwitcher) findViewById(R.id.imgsw);
+
         imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
             @Override
             public View makeView() {
@@ -31,9 +43,15 @@ public class HelpActivity extends AppCompatActivity {
             }
         });
 
+        imageSwitcher.setImageResource(R.drawable.big_logo);
+        previous.setVisibility(View.INVISIBLE);
+
         previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                current_position--;
+                if (current_position == 0)
+                    previous.setVisibility(View.INVISIBLE);
                 imageSwitcher.setImageResource(R.drawable.big_logo);
             }
         });
@@ -41,7 +59,10 @@ public class HelpActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageSwitcher.setImageResource(R.drawable.ic_search);
+                current_position++;
+                if (current_position == 1)
+                    previous.setVisibility(View.VISIBLE);
+                imageSwitcher.setImageResource(R.drawable.big_logo);
             }
         });
     }
