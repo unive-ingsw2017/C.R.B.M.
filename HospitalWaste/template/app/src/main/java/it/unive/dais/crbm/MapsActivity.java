1f995f6e05f3518053c91827c0402855eaf511cd
Francesco Benvenuto <francesco_benvenuto@me.com>
2018-02-01 01:09:15 +0200

    late but only for remove unused code

--- HospitalWaste/template/app/src/main/java/it/unive/dais/crbm/MapsActivity.java
+++ HospitalWaste/template/app/src/main/java/it/unive/dais/crbm/MapsActivity.java
@@ -10,10 +10,8 @@ import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
@@ -38,15 +36,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStates;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -170,25 +162,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // inizializza la mappa asincronamente
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
-        // quando viene premito il pulsante HERE viene eseguito questo codice
-        /**button_here.setOnClickListener(v -> {
-         Log.d(TAG, "here button clicked");
-         gpsCheck();
-         updateCurrentPosition();
-         if (hereMarker != null) hereMarker.remove();
-         if (currentPosition != null) {
-         MarkerOptions opts = new MarkerOptions();
-         opts.position(currentPosition);
-         opts.title(getString(R.string.marker_title));
-         opts.snippet(String.format("lat: %g\nlng: %g", currentPosition.latitude, currentPosition.longitude));
-         hereMarker = gMap.addMarker(opts);
-         if (gMap != null)
-         gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, getResources().getInteger(R.integer.zoomFactor_button_here)));
-         } else
-         Log.d(TAG, "no current position available");
-         });*/
     }
 
     private DrawerLayout drawer;
@@ -493,23 +466,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      */
     @Override
     public void onCameraMoveStarted(int reason) {
-        setHereButtonVisibility();
-    }
-
-    /**
-     * Metodo proprietario che imposta la visibilità del pulsante HERE.
-     * Si occupa di nascondere o mostrare il pulsante HERE in base allo zoom attuale, confrontandolo con la soglia di zoom
-     * impostanta nelle preferenze.
-     * Questo comportamento è dimostrativo e non è necessario tenerlo quando si sviluppa un'applicazione modificando questo template.
-     */
-    public void setHereButtonVisibility() {
-        if (gMap != null) {
-            if (gMap.getCameraPosition().zoom < SettingsActivity.getZoomThreshold(this)) {
-                //button_here.setVisibility(View.INVISIBLE);
-            }/* else {
-                button_here.setVisibility(View.VISIBLE);
-            }*/
-        }
     }
 
 
@@ -545,11 +501,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         UiSettings uis = gMap.getUiSettings();
         uis.setZoomGesturesEnabled(true);
         uis.setMyLocationButtonEnabled(true);
-        //gMap.setOnMyLocationButtonClickListener(
-          //      () -> {
-            //        gpsCheck();
-              //      return false;
-                //à});
 
         uis.setCompassEnabled(true);
         uis.setZoomControlsEnabled(true);
@@ -574,22 +525,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Log.d(TAG, "applying map settings");
             gMap.setMapType(SettingsActivity.getMapStyle(this));
         }
-        setHereButtonVisibility();
-    }
-
-    /**
-     * Naviga dalla posizione from alla posizione to chiamando il navigatore di Google.
-     *
-     * @param from posizione iniziale.
-     * @param to   posizione finale.
-     */
-    protected void navigate(@NonNull LatLng from, @NonNull LatLng to) {
-        Intent navigation = new Intent(
-                Intent.ACTION_VIEW,
-                Uri.parse("http://maps.google.com/maps?saddr=" + from.latitude + "," + from.longitude + "&daddr=" + to.latitude + "," + to.longitude + ""));
-        navigation.setPackage("com.google.android.apps.maps");
-        Log.i(TAG, String.format("starting navigation from %s to %s", from, to));
-        startActivity(navigation);
     }
 
     // marker stuff
@@ -609,20 +544,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
      */
     @Override
     public boolean onMarkerClick(final Marker marker) {
-        /*
-        if(confrontaEnable){
-            confrontaMarkerClick(marker);
-
-            CameraUpdate center = CameraUpdateFactory.newLatLng(marker.getPosition());
-            gMap.animateCamera(
-                    center,
-                    400,    // to move the camera faster
-                    null
-            );
-
-            return true; // we consume the event stop the callbacks
-        }*/
-
         marker.setSnippet(manageMarkerDescription(marker));
         return false; // continue to call back the default staff (zoom, and show info)
     }
@@ -653,47 +574,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
     }
-
-
-    /**
-     * Controlla lo stato del GPS e dei servizi di localizzazione, comportandosi di conseguenza.
-     * Viene usata durante l'inizializzazione ed in altri casi speciali.
-     */
-    protected void gpsCheck() {
-        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(MapsActivity.this).addApi(LocationServices.API).build();
-        googleApiClient.connect();
-
-        LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-        builder.setAlwaysShow(true);
-
-        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
-        result.setResultCallback(
-                result1 -> {
-                    final Status status = result1.getStatus();
-                    switch (status.getStatusCode()) {
-                        case LocationSettingsStatusCodes.SUCCESS:
-                            Log.i(TAG, "All location settings are satisfied.");
-                            break;
-                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                            Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
-                            try {
-                                // Show the dialog by calling startResolutionForResult(), and check the result
-                                // in onActivityResult().
-                                status.startResolutionForResult(MapsActivity.this, REQUEST_CHECK_SETTINGS);
-                            } catch (IntentSender.SendIntentException e) {
-                                Log.i(TAG, "PendingIntent unable to execute request.");
-                            }
-                            break;
-                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                            Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
-                            break;
-                    }
-                });
-    }
-
 
     // putMarkers code
     private void putMarkers() {
